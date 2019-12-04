410015c487f88f15bafc7752be0d7524bd81916f
p-eye <standingtherain15@gmail.com>
2019-12-04 14:45:25 +0900

    CategoryResponse Api
    with jackson 라이브러리

---
+++ src/main/java/kr/or/connect/reservation/controller/CategoryApiController.java
@@ -0,0 +1,24 @@
+package kr.or.connect.reservation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.or.connect.reservation.dto.CategoryResponse;
+import kr.or.connect.reservation.service.CategoryService;
+
+@RestController
+@RequestMapping(path = "/api/categories")
+public class CategoryApiController {
+
+
+	@Autowired
+	public CategoryService categoryService;
+	
+    @GetMapping(path = "")
+    public CategoryResponse getCategoryResponse() {
+        return categoryService.getCategoryResponse();
+    }
+
+}
