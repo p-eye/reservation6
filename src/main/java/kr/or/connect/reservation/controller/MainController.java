3e45dd11d6a64be7ec2dbff6008ab0cea87af980
p-eye <standingtherain15@gmail.com>
2019-12-06 19:13:47 +0900

    review jsp + js 생성

--- src/main/java/kr/or/connect/reservation/controller/MainController.java
+++ src/main/java/kr/or/connect/reservation/controller/MainController.java
@@ -1,6 +1,7 @@
 package kr.or.connect.reservation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,6 +12,11 @@ public class MainController {
 	public String getDetail(@RequestParam(name = "displayInfoId", required = true) int displayInfoId) {
 		return "detail";
 
+	}
+	
+	@GetMapping(path = "/review")
+	public String getReviews(@RequestParam(name = "displayInfoId", required = true) int displayInfoId, ModelMap model) {
+		return "review";
 	}
 
 }
