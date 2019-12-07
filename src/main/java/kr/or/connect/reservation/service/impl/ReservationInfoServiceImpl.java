7a4a91f147fe41145258333fcdf4bbb1dd6dff0f
p-eye <standingtherain15@gmail.com>
2019-12-07 15:53:27 +0900

    ReservationInfo에 setDisplayInfo, setTotalPrice

--- src/main/java/kr/or/connect/reservation/service/impl/ReservationInfoServiceImpl.java
+++ src/main/java/kr/or/connect/reservation/service/impl/ReservationInfoServiceImpl.java
@@ -7,17 +7,34 @@ import org.springframework.stereotype.Service;
 
 import kr.or.connect.reservation.dao.ReservationInfoDao;
 import kr.or.connect.reservation.dto.ReservationInfo;
+import kr.or.connect.reservation.service.DisplayInfoService;
 import kr.or.connect.reservation.service.ReservationInfoService;
+import kr.or.connect.reservation.service.ReservationPriceService;
 
 @Service
 public class ReservationInfoServiceImpl implements ReservationInfoService {
 
 	@Autowired
 	private ReservationInfoDao reservationInfoDao;
+	
+	@Autowired
+	private DisplayInfoService displayInfoService;
+	
+	@Autowired
+	private ReservationPriceService reservationPriceService;
 
 	@Override
 	public List<ReservationInfo> getReservationInfoList(String reservationEmail) {
-		return reservationInfoDao.getReservationInfoList(reservationEmail);
+		List<ReservationInfo> reservationInfoList =	 reservationInfoDao.getReservationInfoList(reservationEmail);
+		
+		//Each ReservationInfo
+		for (ReservationInfo reservationInfo : reservationInfoList) {
+			reservationInfo.setDisplayInfo(displayInfoService.getDisplayInfo(reservationInfo.getDisplayInfoId()));
+			reservationInfo.setTotalPrice(reservationPriceService.getReservationTotalPrice(reservationInfo.getReservationInfoId()));
+		}
+		
+		return reservationInfoList;
+
 	}
 
 }
